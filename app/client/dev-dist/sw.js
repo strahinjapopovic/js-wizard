c91882236042e873d010281f5d581cf853b59f8f
Stan Popovic <spope.mails@gmail.com>
2024-10-09 12:45:37 +1100

    build update

--- app/client/dev-dist/sw.js
+++ app/client/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-a9fe0588'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.u23dj8p0nb8"
+    "revision": "0.83elfrd7bh"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
