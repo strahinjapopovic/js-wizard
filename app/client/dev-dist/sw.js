1f678a81241682f77aade722a8a900d812634a65
Stan Popovic <spope.mails@gmail.com>
2024-10-11 13:03:38 +1100

    test ephemeral data

--- app/client/dev-dist/sw.js
+++ app/client/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-a9fe0588'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.alq2k69feo"
+    "revision": "0.rbjubv47voo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
